4b322f8414944910373efb2b314baf734a7ef51c
Nauf <naufpsychic@gmail.com>
2023-07-26 01:31:41 +0500

    fix: add knowledge base to mobile view

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -73,6 +73,10 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+
+            <x-responsive-nav-link :href="route('knowledge-base.index')" :active="request()->routeIs('knowledge-base.index')">
+                {{ __('Knowledge Base') }}
+            </x-responsive-nav-link>
         </div>
 
         <!-- Responsive Settings Options -->
